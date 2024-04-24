1bf4289a72e0619ed96889ff9d174ce532a10a37
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-04-24 05:03:16 +0300

    Update Ss.js

--- Ss.js
+++ Ss.js
@@ -1,1 +1,2 @@
-
+#buyhack
+http-response ^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ script-path=https://raw.githubusercontent.com/PoetryU/Scientist/master/Scripts/langkhach/verify_receipt.js
